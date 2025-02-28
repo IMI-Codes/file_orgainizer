cdde9f3bbe215eef2cac152f54661c8dfc0aa6b2
Manasseh <rougerapper@gmail.com>
2025-02-28 06:59:37 +0100

    I'm having issues with the mkdir function will get round that later

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from os import chdir, getcwd, listdir, mkdir
-from os.path import exists
+from os.path import exists, isdir, join
 from shutil import move
 from re import compile, search, I
 
 
 dir_name = r"C:\Users\rouge\Downloads"
-pahe_pattern = compile(r"^(animepahe)_(\w+)_-_(\d{1,4})_(\d{3,4}p)", I)
-
+pahe_pattern = compile(
+    r"^(animepahe)_(\w+)_-_(\d{1,4})_(\d{3,4}p)", I
+)  # modify for anime with season numbers
+# pattern for all anime
+# account for eps with eng_dub and remove it
+holder = dict()
 if dir_name:
     valid_dir = exists(dir_name)
     if valid_dir == True:
@@ -19,7 +23,30 @@ current_dir = getcwd()
 directory_content = listdir(current_dir)
 for value in directory_content:
     data = pahe_pattern.search(value)
+    # print(data)
+    if data != None:
+        anime_name = data.group(2)
+        anime_name_ep = f"{data.group(2)} {data.group(3)}"
+        print(anime_name_ep)
+        if anime_name in holder:
+            holder[anime_name] += 1
+        else:
+            holder[anime_name] = 1
 
+chdir(r"C:\Users\rouge\Videos")
+directories_and_files = listdir()
+base_path = getcwd()
+for value in holder:
+    if value in directories_and_files and isdir(value) == True:
+        del holder[value]  # this means there is a
+        # add episode to folder and or return folder name
+    else:
+        f_path = join(base_path, value)
+        mkdir(f_path)
+
+# also check if there are episode here too
+
+# check for any videos with similar names but increasing counts
 
 # checks all folder for content
 
