e213476e7690d18377bb7143fb2599edca4097d5
Manasseh <rougerapper@gmail.com>
2025-02-28 23:51:58 +0100

    I'm refactoring the code to make it better but from my next session I'm just going to go with it the goal is to get something that works

--- main.py
+++ main.py
@@ -1,54 +1,22 @@
-from os import chdir, getcwd, listdir, mkdir
-from os.path import exists, isdir, join
-from shutil import move
+from os import chdir, getcwd, listdir
+from os.path import isfile
 from re import compile, search, I
 
+downloads_dir = r"C:\Users\rouge\Downloads"
+# change to  Downloads
+chdir(downloads_dir)
+# get a list of all files and folders
+dir_contents = listdir()
+# filter out and return only the videos
+video_pattern = compile(r"\.(mkv|mp4|webm)$")
 
-dir_name = r"C:\Users\rouge\Downloads"
-pahe_pattern = compile(
-    r"^(animepahe)_(\w+)_-_(\d{1,4})_(\d{3,4}p)", I
-)  # modify for anime with season numbers
-# pattern for all anime
-# account for eps with eng_dub and remove it
-holder = dict()
-if dir_name:
-    valid_dir = exists(dir_name)
-    if valid_dir == True:
-        chdir(dir_name)
-else:
-    raise Exception
-current_dir = getcwd()
+videos = list()
+for item in dir_contents:
+    if isfile(item) == True:
+        video = video_pattern.search(item)
+        if video != None:
+            videos.append(item)
 
-# check for all files and folders in the directory
-directory_content = listdir(current_dir)
-for value in directory_content:
-    data = pahe_pattern.search(value)
-    # print(data)
-    if data != None:
-        anime_name = data.group(2)
-        anime_name_ep = f"{data.group(2)} {data.group(3)}"
-        print(anime_name_ep)
-        if anime_name in holder:
-            holder[anime_name] += 1
-        else:
-            holder[anime_name] = 1
+# get names of the videos
 
-chdir(r"C:\Users\rouge\Videos")
-directories_and_files = listdir()
-base_path = getcwd()
-for value in holder:
-    if value in directories_and_files and isdir(value) == True:
-        del holder[value]  # this means there is a
-        # add episode to folder and or return folder name
-    else:
-        f_path = join(base_path, value)
-        mkdir(f_path)
-
-# also check if there are episode here too
-
-# check for any videos with similar names but increasing counts
-
-# checks all folder for content
-
-
-# save when last it run to a file  and when last the file was updated
+# go to videos and return only the folders
