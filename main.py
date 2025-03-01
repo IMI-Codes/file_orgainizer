83c20f3a55fe13b2c3dbcc6666671983e3ee31e5
Ishima Manasseh <rougerapper@gmail.com>
2025-03-01 20:42:12 +0100

    another one down

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from os import chdir, getcwd, listdir, mkdir
-from os.path import isfile, isdir
+from os import chdir, getcwd, listdir, mkdir, makedirs
+from os.path import isfile, isdir, join
 from re import compile, search, I, sub
 
 downloads_dir = r"C:\Users\rouge\Downloads"
-# change to  Downloads
 chdir(downloads_dir)
-# get a list of all files and folders
 dir_contents = listdir()
-# filter out and return only the videos
 video_pattern = compile(r"\.(mkv|mp4|webm)$")
 
 videos = list()
@@ -17,24 +14,33 @@ for item in dir_contents:
         if video != None:
             videos.append(item)
 
+print(videos)
 
-# get names of the videos
-pahe_check_pattern = compile(r"^animepahe", I)
-anime_name_pattern = compile(r"_(\w+)_-_", I)
+# patterns
+fz_pattern = compile(r"(\w+)_-_(\w+)_")
+pahe_check_pattern = compile(r"^animepahe_(\w+)_-_", I)
+# anime_name_pattern = compile(r"_(\w+)_-_", I)
 dub_check = compile(r"_eng_dub", I)
+
+
 animes = dict()
+
+
 for vid in videos:
     pahe_video = pahe_check_pattern.search(vid)
+    fz_video = fz_pattern.search(vid)
     if pahe_video != None:
-        anime = anime_name_pattern.search(vid)
+        anime = pahe_check_pattern.search(vid)
         if anime != None:
             anime_name = anime.group(1)
-            # account for dubs too
             anime_name = dub_check.sub(r"", anime_name)  # returns the name without dub
             if anime_name in animes:
                 animes[anime_name] += 1
             else:
                 animes[anime_name] = 1
+    elif fz_video != None:
+        print(fz_video)
+
 
 print(animes)
 # other videos name
@@ -52,13 +58,15 @@ for value in vid_dir_contents:
         vid_dir_folders.append(value)
 
 # cross check names in folders
+# make a folder for movies,animes,series also pass my watchlist so it can it tell which is which
 for name in animes:
     if name not in vid_dir_folders:
-        dir_name = name
-        mkdir(dir_name)
+        base_path = videos_dir
+        final_path = join(base_path, name)
+        # mkdir(final_path)
         # create a new directory and add the anime to that folder
     else:
-        pass  # add the anime to that folder
+        pass  # add the anime to that folder get file name and pass move here
 
 
 # use ep count and copy or 2 to identify duplicates
