5f79134ea1ce3f19ecfe5c28bcbed6fb8657ca59
Ishima Manasseh <rougerapper@gmail.com>
2025-03-01 20:41:35 +0100

    making progress mkdir is not behaving how I expect so we'll see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from os import chdir, getcwd, listdir
-from os.path import isfile
-from re import compile, search, I
+from os import chdir, getcwd, listdir, mkdir
+from os.path import isfile, isdir
+from re import compile, search, I, sub
 
 downloads_dir = r"C:\Users\rouge\Downloads"
 # change to  Downloads
@@ -17,6 +17,48 @@ for item in dir_contents:
         if video != None:
             videos.append(item)
 
-# get names of the videos
 
+# get names of the videos
+pahe_check_pattern = compile(r"^animepahe", I)
+anime_name_pattern = compile(r"_(\w+)_-_", I)
+dub_check = compile(r"_eng_dub", I)
+animes = dict()
+for vid in videos:
+    pahe_video = pahe_check_pattern.search(vid)
+    if pahe_video != None:
+        anime = anime_name_pattern.search(vid)
+        if anime != None:
+            anime_name = anime.group(1)
+            # account for dubs too
+            anime_name = dub_check.sub(r"", anime_name)  # returns the name without dub
+            if anime_name in animes:
+                animes[anime_name] += 1
+            else:
+                animes[anime_name] = 1
+
+print(animes)
+# other videos name
 # go to videos and return only the folders
+videos_dir = r"C:\Users\rouge\Videos"
+chdir(videos_dir)
+
+vid_dir_contents = listdir()
+# handle files to check if there's videos with no folder
+vid_dir_folders = list()
+for value in vid_dir_contents:
+    if isdir(value) == False:
+        continue
+    else:
+        vid_dir_folders.append(value)
+
+# cross check names in folders
+for name in animes:
+    if name not in vid_dir_folders:
+        dir_name = name
+        mkdir(dir_name)
+        # create a new directory and add the anime to that folder
+    else:
+        pass  # add the anime to that folder
+
+
+# use ep count and copy or 2 to identify duplicates
