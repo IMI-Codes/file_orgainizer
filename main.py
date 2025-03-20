c22b77990a09b9251b022e0284f0525900d9ae0f
Ishima Manasseh <rougerapper@gmail.com>
2025-03-20 15:48:06 +0100

    having issues with the os.makedirs and mkdir function I need to make sure there is always a directory to save my log files to

--- main.py
+++ main.py
@@ -16,8 +16,20 @@ timestamp = datetime.now()
 downloads_path = r"C:\Users\rouge\Downloads"
 contents = change_dir(downloads_path)
 documents_path = r"C:\Users\rouge\OneDrive\Documents"
-logs_path = join(documents_path, r"CustomSystemLogs\file_organizer_logs")
+logs_path = join(documents_path, r"CustomSystemLogs")
 
+# r"CustomSystemLogs/file_organizer_logs"
+
+# |||||||||||| CREATING A LOG FILE FOR EACH TIME THE CODE RUNS ||||||||||||||||||||||||||||||
+logs_path_valid = exists(logs_path)
+print(logs_path_valid)
+if logs_path_valid == False:
+    makedirs(logs_path)
+else:
+    pass
+# Create a log file immediately the code starts to run
+# check if the dir is there
+# create a custom log for that date and time
 
 # ||||||||||||| DOWNLOADS DIRECTORY |||||||||||||||
 unknown_dir_path = join(downloads_path, "unknown")
@@ -39,4 +51,29 @@ all_files = get_files_from_folders(folders, downloads_path) + files
 file_name_and_extension = get_extension_file_name(all_files)
 
 
-determine_cat(file_name_and_extension)  # type: ignore
+sorted_files = determine_cat(file_name_and_extension)  # type: ignore
+
+if type(sorted_files) == tuple:
+    sorted_files_cat, unknown_f_types = sorted_files
+elif type(sorted_files) == dict:
+    if "Message" in sorted_files:
+        unknown_f_types = sorted_files
+    else:
+        sorted_files_cat = sorted_files
+
+if unknown_f_types:  # type: ignore
+    for f in unknown_f_types:
+        pass
+        # move(f, unknown_dir_path)  # type: ignore
+
+# move the files to the there respective directories
+# move the unknown to the unknown dir
+# move all the known to the staging dir
+# Update the logs files
+# 1. Unknown files
+# 2. Files moved and to which directory
+
+
+# clean up downloads directory
+
+# VIDEOS DIRECTORY
